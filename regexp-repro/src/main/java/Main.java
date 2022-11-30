66cbbdd3eff6a42645b37e0742ba5d24f1f36c77
Andy Grove <andygrove73@gmail.com>
2022-11-30 09:50:34 -0700

    compare cpu and gpu

--- regexp-repro/src/main/java/Main.java
+++ regexp-repro/src/main/java/Main.java
@@ -1,113 +1,49 @@
 import ai.rapids.cudf.ColumnVector;
-import ai.rapids.cudf.DType;
 import ai.rapids.cudf.HostColumnVector;
-import ai.rapids.cudf.Table;
 
-import java.util.Arrays;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class Main {
 
-  static final String REGEX_PATTERN = "(.|\\n)*1(.|\\n)0(.|\\n)*";
+  public static void main(String args[]) {
 
-  public static void main(String args[]) throws InterruptedException {
-    doStabilityTest(166_666_667/16, 2, 10);
+    String[] inputs = {
+            "hello\n",
+            "hell\no\n",
+            "hell\no\n\n",
+            "goodbye",
+    };
+
+    String pattern = "o$";
+
+    System.out.println("Pattern = " + pattern);
+
+    boolean[] cpu = cpuContains(pattern, inputs);
+    boolean[] gpu = gpuContains(pattern, inputs);
+
+    for (int i=0; i<inputs.length; i++) {
+      System.out.println(inputs[i] + " -> cpu=" + cpu[i] + "; gpu=" + gpu[i]);
+    }
   }
 
-  static void doStabilityTest(int n, int numThreads, int maxAttempt) throws InterruptedException {
-    // This test aims to reproduce the following Spark query, which produces
-    // inconsistent results between runs:
-    //
-    // spark.range(1000000000L)
-    //   .selectExpr("CAST(id as STRING) as str_id")
-    //   .filter("regexp_like(str_id, '(.|\n)*1(.|\n)0(.|\n)*')")
-    //   .count()
-
-    // create the input data once in host memory
-    long[][] array = new long[numThreads][];
-    for (int threadNo=0; threadNo<numThreads; threadNo++) {
-      int chunk_size = n / numThreads;
-      array[threadNo] = new long[chunk_size];
-      long[] x = array[threadNo];
-      for (int i = 0; i < chunk_size; i++) {
-        x[i] = (long) chunk_size * threadNo + i;
-      }
+  private static boolean[] cpuContains(String pattern, String[] inputs) {
+    Pattern p = Pattern.compile(pattern);
+    boolean ret[] = new boolean[inputs.length];
+    for (int i=0; i<ret.length; i++) {
+      ret[i] = p.matcher(inputs[i]).find(0);
     }
-
-    String csvResults[] = new String[maxAttempt];
-    for (int attempt = 0; attempt< maxAttempt; attempt++) {
-      System.err.println("attempt " + attempt + " of " + maxAttempt);
-
-      Thread threads[] = new Thread[numThreads];
-      long result[] = new long[numThreads];
-      for (int j = 0; j < numThreads; j++) {
-        int threadNo = j;
-        threads[j] = new Thread(() -> {
-
-          // cast longs to string then release the longs vector
-          ColumnVector castTo;
-          try (ColumnVector longs = ColumnVector.fromLongs(array[threadNo])) {
-               castTo = longs.castTo(DType.STRING);
-          }
-
-          // Java (CPU) matchesRe
-          int cpuCount = 0;
-          Pattern p = Pattern.compile(REGEX_PATTERN);
-          HostColumnVector hv = castTo.copyToHost();
-          for (int i = 0; i < hv.getRowCount(); i++) {
-            if (p.matcher(hv.getJavaString(i)).matches()) {
-              cpuCount += 1;
-            }
-          }
-          hv.close();
-
-//          int nn = 10;
-//          for (int i = 0; i< nn; i++) {
-//            System.err.println("match["+ i + "] " + hv.getJavaString(i));
-//          }
-//          for (int i = (int)t2.getRowCount() - nn -1; i<t2.getRowCount(); i++) {
-//            System.err.println("match["+ i + "] " + hv.getJavaString(i));
-//          }
-
-
-          // matchesRe and filter
-          try (ColumnVector matchesRe = castTo.containsRe(REGEX_PATTERN);
-               Table t = new Table(castTo);
-               Table t2 = t.filter(matchesRe)) {
-            result[threadNo] = t2.getRowCount();
-            System.err.println("thread " + threadNo + " gpuCount: " + t2.getRowCount() + " cpuCount: " + cpuCount);
-
-
-          }
-
-        });
-
-      }
-
-      // start the threads
-      for (int j = 0; j < numThreads; j++) {
-        threads[j].start();
-      }
-
-      for (Thread t : threads) {
-        t.join();
-      }
-
-      csvResults[attempt] = Arrays.stream(result)
-              .mapToObj(String::valueOf)
-              .collect(Collectors.joining(","));
-    }
-
-    // check that the results from each run are the same
-    for (int attempt = 0; attempt< maxAttempt; attempt++) {
-      System.out.println(csvResults[attempt]);
-      if (!csvResults[0].equals(csvResults[attempt])) {
-        throw new IllegalStateException();
-      }
-    }
-
+    return ret;
   }
 
+  private static boolean[] gpuContains(String pattern, String[] inputs) {
+    ColumnVector cv = ColumnVector.fromStrings(inputs);
+    ColumnVector cv2 = cv.containsRe(pattern);
+    HostColumnVector hostColumnVector = cv2.copyToHost();
+    boolean ret[] = new boolean[inputs.length];
+    for (int i=0; i<ret.length; i++) {
+      ret[i] = hostColumnVector.getBoolean(i);
+    }
+    return ret;
+  }
 }
 
